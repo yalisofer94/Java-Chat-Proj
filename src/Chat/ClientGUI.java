7471f7ed29fe574f58503595040844dbc0e52cca
yalisofer94 <yalisofer@gmail.com>
2020-12-26 16:01:28 +0200

    staring gui

--- src/Chat/ClientGUI.java
+++ src/Chat/ClientGUI.java
@@ -1,10 +1,19 @@
 package Chat;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ClientGUI implements StringConsumer, StringProducer
 {
+    private JFrame frame;
+    private JPanel panelOne, panelTwo;
+    private JButton btPlus;
+    private JTextField tf1,tf2;
+
+
     @Override
     public void consume(String str) {
-    
+
     }
 
     @Override
